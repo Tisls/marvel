d5a042ffb6b9950275edd2707e1825cd450e2aad
t1sl <trafim.brokh@gmail.com>
2024-01-13 22:19:39 +0000

    side bar

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -29,7 +29,8 @@ class MarvelService {
                 "There is no description for this character", //use slice to reduce text max if description = true else print text
             thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url
+            wiki: char.urls[1].url,
+            comics: char.comics.items
         };
     }
 
